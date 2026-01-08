975b0cf81c40e081121b003dde05c1945adf8d54
CarlosOrellana00 <carlosorellana.oa@gmail.com>
2026-01-08 01:16:02 -0400

    barra jugador

--- juego.py
+++ juego.py
@@ -30,14 +30,42 @@ class Bolita(pygame.sprite.Sprite):
     # Mover en base a posicion actual y velocidad
     self.rect.move_ip(self.speed)
 
+class Paleta(pygame.sprite.Sprite):
+  def __init__(self):
+    pygame.sprite.Sprite.__init__(self)
+    # Cargar Imagen
+    self.image = pygame.image.load("imagenes/paleta.png")
+    # Escalar la imagen a 20x20 píxeles
+    self.image = pygame.transform.scale(self.image, (60, 20))
+    # Obtener rectangulo de la imagen
+    self.rect = self.image.get_rect()
+    # Posición inicial centrada en x
+    self.rect.midbottom = (ancho /2, alto -20)
+    # Establecer velocidad inicial
+    self.speed = [0,0]
+  def update(self,evento):
+    # Buscar si se preciono flecha izquierda
+    if evento.key == pygame.K_LEFT and self.rect.left > 0:
+      self.speed = [-5,0]
+    # si se preciono la flecha derecha
+    elif evento.key == pygame.K_RIGHT and self.rect.right < ancho:
+      self.speed = [5,0]
+    else:
+      self.speed = [0,0]
+    # Mover en base a posicion actual y velocidad
+    self.rect.move_ip(self.speed)
+
 # Inicializando pantalla
 pantalla = pygame.display.set_mode((ancho, alto))
 # Configurar titulo de pantalla
 pygame.display.set_caption("Juego de Ladrillos")
 # Crear Reloj
 reloj = pygame.time.Clock()
+# Ajustar repeticion de evento de tecla presionada
+pygame.key.set_repeat(30)
 
 bolita = Bolita()
+jugador = Paleta()
 
 while True:
   # Establecer FPS
@@ -45,15 +73,21 @@ while True:
 
   # Revisar todos los eventos
   for evento in pygame.event.get():
+    # Si se presiona la barra de titulo
     if evento.type == pygame.QUIT:
       sys.exit()
+    # Buscar eventos del teclado
+    elif evento.type == pygame.KEYDOWN:
+      jugador.update(evento)
 
   # Actualizar posicion de la bolita
   bolita.update()
   # Rellenar la pantalla.
   pantalla.fill(color_azul)
-  #Dibujar la bolita en pantalla
+  # Dibujar la bolita en pantalla
   pantalla.blit(bolita.image, bolita.rect)
+  # Dibujar jugador en pantalla
+  pantalla.blit(jugador.image, jugador.rect)
   # Actualizar los elementos en pantalla
   pygame.display.flip()
 
