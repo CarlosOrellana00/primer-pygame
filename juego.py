216ada5783575b18d0f49b5727f60ef61a8f4808
CarlosOrellana00 <carlosorellana.oa@gmail.com>
2026-01-08 00:39:52 -0400

    bolita atrapada

--- juego.py
+++ juego.py
@@ -3,6 +3,7 @@ import pygame
 
 ancho = 640
 alto = 480
+color_azul = (0,0,64) #Color azul para el fondo.
 
 class Bolita(pygame.sprite.Sprite):
   def __init__(self):
@@ -16,20 +17,41 @@ class Bolita(pygame.sprite.Sprite):
     # Posición inicial centrada en pantalla
     self.rect.centerx = ancho /2
     self.rect.centery = alto /2
+    # Establecer velocidad inicial
+    self.speed = [3,3]
+
+  def update(self):
+    # Evitar que salga por arriba o abajo
+    if self.rect.bottom >= alto or self.rect.top <= 0:
+      self.speed[1] = -self.speed[1]
+    #  Evitar que salga por la derecha o izquierda
+    elif self.rect.right >= ancho or self.rect.left <= 0:
+      self.speed[0] = -self.speed[0]
+    # Mover en base a posicion actual y velocidad
+    self.rect.move_ip(self.speed)
 
 # Inicializando pantalla
 pantalla = pygame.display.set_mode((ancho, alto))
 # Configurar titulo de pantalla
 pygame.display.set_caption("Juego de Ladrillos")
+# Crear Reloj
+reloj = pygame.time.Clock()
 
 bolita = Bolita()
 
 while True:
+  # Establecer FPS
+  reloj.tick(60)
+
   # Revisar todos los eventos
   for evento in pygame.event.get():
     if evento.type == pygame.QUIT:
       sys.exit()
 
+  # Actualizar posicion de la bolita
+  bolita.update()
+  # Rellenar la pantalla.
+  pantalla.fill(color_azul)
   #Dibujar la bolita en pantalla
   pantalla.blit(bolita.image, bolita.rect)
   # Actualizar los elementos en pantalla
